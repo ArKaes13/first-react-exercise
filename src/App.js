ccf5d1eb6c75c4bd9da97a8cb6fcbe96fdc58476
Tyler <ArKaes1820@yahoo.com>
2023-04-04 17:05:51 -0700

    first step of the exercise completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,38 @@ import React from 'react'
 
 function Header() {
   return (
-    <h1>Hello</h1>
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  )
+}
+
+function Note() {
+  return (
+    <div className='note'>
+      <h1>Note title</h1>
+      <p>Note Content</p>
+    </div>
+  )
+}
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>&copy; {currentYear} Tyler Montgomery</p>
+    </footer>
   )
 }
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
   )
 }
 
