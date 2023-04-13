b2916ec8a01b34cce2b569c308d8dcd883c82650
Tyler <ArKaes1820@yahoo.com>
2023-04-13 16:44:01 -0700

    Used props and .map to create content from array

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Header from './Header.js'
 import Note from './Note.js'
 import Footer from './Footer.js'
+import notes from '../notes.js'
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map( (note) => {
+        return (
+          <Note 
+            title={note.title}
+            content={note.content}
+          />
+        )
+      })}
       <Footer />
     </div>
   )
