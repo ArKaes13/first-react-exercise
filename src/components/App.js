8bac368e9e38c97944c68aed095d3f86ce0a7d85
Tyler <ArKaes1820@yahoo.com>
2023-06-06 21:26:16 -0700

    Added functionality to add and delete notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header.js'
 import Note from './Note.js'
 import Footer from './Footer.js'
-import notes from '../notes.js'
+import CreateArea from './CreateArea'
 
 function App() {
+  const [notes, setNotes] = useState([])
+  
+  function addNote(note) {
+    setNotes( (prevNotes) => {
+      return [...prevNotes, note];
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes( (prevNotes) => {
+      return prevNotes.filter( (note, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      {notes.map( (note) => {
+      <CreateArea
+        onAdd={addNote} 
+      />
+      {notes.map( (noteItem, index) => {
         return (
-          <Note 
-            title={note.title}
-            content={note.content}
-          />
-        )
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />)
       })}
       <Footer />
     </div>
